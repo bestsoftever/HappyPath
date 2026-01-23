8e11a7f3c98fa3b8f1aad3126f973b8d1d380d60
bestsoftever <me@bestsoftever.dev>
2026-01-23 02:37:51 +0100

    Tests improved

--- integrations/HappyPath.AwesomeAssertions.Tests/BeErrorTests.cs
+++ integrations/HappyPath.AwesomeAssertions.Tests/BeErrorTests.cs
@@ -3,6 +3,11 @@ using Xunit.Sdk;
 
 namespace HappyPath.AwesomeAssertions.Tests;
 
+public class TestError(string message, string content = "default content") : Error(message)
+{
+	public string Content { get; } = content;
+}
+
 public class BeErrorTests
 {
 	[Fact]
@@ -16,19 +21,9 @@ public class BeErrorTests
 	}
 
 	[Fact]
-	public void NoParams_DifferentError_Passes()
-	{
-		Result<string> result = new Error("different error");
-
-		var act = () => result.Should().BeError();
-
-		act.Should().NotThrow();
-	}
-
-	[Fact]
 	public void NoParams_Error_Passes()
 	{
-		Result<string> result = new Error("error!");
+		Result<string> result = new TestError("error!");
 
 		var act = () => result.Should().BeError();
 
@@ -40,7 +35,7 @@ public class BeErrorTests
 	{
 		Result<string> result = "valid text";
 
-		var act = () => result.Should().BeError(new Error("error!"));
+		var act = () => result.Should().BeError(new TestError("error!"));
 
 		act.Should().Throw<XunitException>();
 	}
@@ -48,7 +43,7 @@ public class BeErrorTests
 	[Fact]
 	public void WithExpected_DifferentError_Fails()
 	{
-		Result<string> result = new Error("wrong error");
+		Result<string> result = new TestError("wrong error");
 
 		var act = () => result.Should().BeError(new Error("error!"));
 
@@ -58,7 +53,7 @@ public class BeErrorTests
 	[Fact]
 	public void WithExpected_SameError_Passes()
 	{
-		Result<string> result = new Error("error!");
+		Result<string> result = new TestError("error!");
 
 		var act = () => result.Should().BeError(new Error("error!"));
 
@@ -80,17 +75,25 @@ public class BeErrorTests
 	{
 		Result<string> result = new Error("wrong error");
 
-		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
+		var act = () => result.Should().BeError(error =>
+		{
+			error.Should().BeOfType<TestError>();
+			error.Should().BeEquivalentTo(new TestError("error!"));
+		});
 
 		act.Should().Throw<XunitException>();
 	}
 
 	[Fact]
-	public void WithAction_SameErrorMessages_Passes()
+	public void WithAction_SameError_Passes()
 	{
 		Result<string> result = new Error("error!");
 
-		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
+		var act = () => result.Should().BeError(error =>
+		{
+			error.Should().BeOfType<TestError>();
+			error.Should().BeEquivalentTo(new TestError("error!"));
+		});
 
 		act.Should().NotThrow();
 	}
