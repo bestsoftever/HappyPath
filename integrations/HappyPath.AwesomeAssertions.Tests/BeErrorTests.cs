17276a67d4546e8b7686c379ed9e70827f161fb3
bestsoftever <me@bestsoftever.dev>
2026-01-23 02:25:33 +0100

    Tests reordered

--- integrations/HappyPath.AwesomeAssertions.Tests/BeErrorTests.cs
+++ integrations/HappyPath.AwesomeAssertions.Tests/BeErrorTests.cs
@@ -6,36 +6,6 @@ namespace HappyPath.AwesomeAssertions.Tests;
 public class BeErrorTests
 {
 	[Fact]
-	public void WithAction_ValidValue_Fails()
-	{
-		Result<string> result = "valid text";
-
-		var act = () => result.Should().BeError(_ => { });
-
-		act.Should().Throw<XunitException>();
-	}
-
-	[Fact]
-	public void WithAction_DifferentError_Fails()
-	{
-		Result<string> result = new Error("wrong error");
-
-		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
-
-		act.Should().Throw<XunitException>();
-	}
-
-	[Fact]
-	public void WithAction_SameErrorMessages_Passes()
-	{
-		Result<string> result = new Error("error!");
-
-		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
-
-		act.Should().NotThrow();
-	}
-
-	[Fact]
 	public void NoParams_ValidValue_Fails()
 	{
 		Result<string> result = "valid text";
@@ -91,6 +61,36 @@ public class BeErrorTests
 		Result<string> result = new Error("error!");
 
 		var act = () => result.Should().BeError(new Error("error!"));
+
+		act.Should().NotThrow();
+	}
+
+	[Fact]
+	public void WithAction_ValidValue_Fails()
+	{
+		Result<string> result = "valid text";
+
+		var act = () => result.Should().BeError(_ => { });
+
+		act.Should().Throw<XunitException>();
+	}
+
+	[Fact]
+	public void WithAction_DifferentError_Fails()
+	{
+		Result<string> result = new Error("wrong error");
+
+		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
+
+		act.Should().Throw<XunitException>();
+	}
+
+	[Fact]
+	public void WithAction_SameErrorMessages_Passes()
+	{
+		Result<string> result = new Error("error!");
+
+		var act = () => result.Should().BeError(error => error.Message.Should().Be("error!"));
 
 		act.Should().NotThrow();
 	}
--- integrations/HappyPath.AwesomeAssertions.Tests/BeValidTests.cs
+++ integrations/HappyPath.AwesomeAssertions.Tests/BeValidTests.cs
@@ -6,36 +6,6 @@ namespace HappyPath.AwesomeAssertions.Tests;
 public class BeValidTests
 {
 	[Fact]
-	public void WithAction_Error_Fails()
-	{
-		Result<string> result = new Error("error!");
-
-		var act = () => result.Should().BeValid(_ => { });
-
-		act.Should().Throw<XunitException>();
-	}
-
-	[Fact]
-	public void WithAction_DifferentValue_Fails()
-	{
-		Result<string> result = "wrong text";
-
-		var act = () => result.Should().BeValid(value => value.Should().Be("valid text"));
-
-		act.Should().Throw<XunitException>();
-	}
-
-	[Fact]
-	public void WithAction_ValidValue_Passes()
-	{
-		Result<string> result = "valid text";
-
-		var act = () => result.Should().BeValid(value => value.Should().Be("valid text"));
-
-		act.Should().NotThrow();
-	}
-
-	[Fact]
 	public void NoParams_Error_Fails()
 	{
 		Result<string> result = new Error("error!");
@@ -91,6 +61,36 @@ public class BeValidTests
 		Result<string> result = "valid text";
 
 		var act = () => result.Should().BeValid("valid text", because: "");
+
+		act.Should().NotThrow();
+	}
+
+	[Fact]
+	public void WithAction_Error_Fails()
+	{
+		Result<string> result = new Error("error!");
+
+		var act = () => result.Should().BeValid(_ => { });
+
+		act.Should().Throw<XunitException>();
+	}
+
+	[Fact]
+	public void WithAction_DifferentValue_Fails()
+	{
+		Result<string> result = "wrong text";
+
+		var act = () => result.Should().BeValid(value => value.Should().Be("valid text"));
+
+		act.Should().Throw<XunitException>();
+	}
+
+	[Fact]
+	public void WithAction_ValidValue_Passes()
+	{
+		Result<string> result = "valid text";
+
+		var act = () => result.Should().BeValid(value => value.Should().Be("valid text"));
 
 		act.Should().NotThrow();
 	}
