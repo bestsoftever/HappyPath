a78fd8f981ba70726bb1c32a421b6506a1a35cac
bestsoftever <me@bestsoftever.dev>
2026-02-09 01:36:07 +0100

    Create HappyPath.AwesomeAssertions integration package (#57)
    Closes #51

---
+++ integrations/HappyPath.AwesomeAssertions/ResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AwesomeAssertions.Execution;
+
+namespace HappyPath.AwesomeAssertions;
+
+public static class ResultExtensions
+{
+	public static ResultAssertions<T> Should<T>(this Result<T> result)
+	{
+		return new ResultAssertions<T>(result, AssertionChain.GetOrCreate());
+	}
+}
